b44dc2e78602cce2a708a54b91a101e02fa24de1
_xoxo_ <aanch.rai@gmail.com>
2021-10-17 16:52:00 -0700

    Create fileioTest2.sh

---
+++ fileioTest2.sh
@@ -0,0 +1,9 @@
+n=1
+while [ $n -lt 6 ]
+do
+      sysbench --test=fileio --file-total-size=2G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct prepare
+      sysbench --test=fileio --file-total-size=2G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct run
+      sleep 60
+      sysbench --test=fileio --file-total-size=2G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct cleanup
+      n=`expr $n + 1`
+done
